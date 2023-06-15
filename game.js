bf3c56691ed40ac557cb54c85552b00a710e79cb
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-06-15 09:39:32 -0500

    finished game function, basic functioning

--- game.js
+++ game.js
@@ -24,18 +24,64 @@ function playRound(playerSelection, computerSelection) {
     } else if (computerSelection == 0 && playerSelection == 2) {
         computerSelection = 3;
     }
-    console.log(playerSelection);
-    console.log(computerSelection);
-    if (playerSelection > computerSelection) {
+    let winner = getWinner(playerSelection, computerSelection);
+    if (winner > 0) {
         return `You win, ${translateIndex(playerSelection)} beats ${translateIndex(computerSelection)}`;
-    } else if (computerSelection > playerSelection) {
+    } else if (winner < 0) {
         return `You lose, ${translateIndex(computerSelection)} beats ${translateIndex(playerSelection)}`
     } else {
         return "Tie"
     }
 }
 
+function getPlayerChoice() {
+    let player = window.prompt("Choose your play");
+    switch (player.toLowerCase()) {
+        case "rock":
+            return 0;
+            break;
+        case "paper":
+            return 1;
+            break;
+        case "scissor":
+            return 2;
+            break;
+    }
+}
 
-let player = 2;
-let computer = getComputerChoice();
-console.log(playRound(player, computer));
+function getWinner(playerSelection, computerSelection) {
+    if (playerSelection == 0 && computerSelection == 2) {
+        playerSelection = 3;
+    } else if (computerSelection == 0 && playerSelection == 2) {
+        computerSelection = 3;
+    }
+    
+    return playerSelection - computerSelection;
+}
+
+function game() {
+    let playerWin = 0;
+    let computerWin = 0;
+    let playerChoice = null;
+    let computerChoice = null;
+
+    
+    let index = 0;
+    while (index < 5) {
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+        let winner = getWinner(playerChoice, computerChoice);
+        if(winner > 0) {
+            ++playerWin;
+        } else if (winner < 0) {
+            ++computerWin;
+        }
+        ++index;
+    }
+
+    console.log(`You won ${playerWin} rounds.`);
+    console.log(`Computer won ${computerWin} rounds.`);
+}
+
+game();
