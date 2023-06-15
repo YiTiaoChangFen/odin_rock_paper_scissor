be9bb7c58f13cbc07a46f60034c555918e6d52f9
YiTiaoChangFen <liangbinz1599043@gmail.com>
2023-06-14 23:14:06 -0500

    finished getComputerChoice function

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let choiceIndex = Math.floor(Math.random() * 3);
+    console.log(choiceIndex);
+
+    switch(choiceIndex) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissor";
+            break;
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
